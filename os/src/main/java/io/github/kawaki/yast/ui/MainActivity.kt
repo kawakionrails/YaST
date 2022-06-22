d8ad6a230bd40ea30b469c9f8b4d4d4d7efb4500
kawakionrails <erick_nara@icloud.com>
2022-06-22 19:35:10 -0300

    add: Hilt (dependency injection)

--- os/src/main/java/io/github/kawaki/yast/ui/MainActivity.kt
+++ os/src/main/java/io/github/kawaki/yast/ui/MainActivity.kt
@@ -1,9 +1,11 @@
-package io.github.kawaki.yast
+package io.github.kawaki.yast.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import io.github.kawaki.yast.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
---
+++ os/src/main/java/io/github/kawaki/yast/ui/MainViewModel.kt
@@ -0,0 +1,12 @@
+package io.github.kawaki.yast.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+
+    // TODO: Not yet implemented
+
+}
