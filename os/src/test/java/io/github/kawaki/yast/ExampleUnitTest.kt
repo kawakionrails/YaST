9b390701c6e3372812fee48a40daea6148b71fd7
kawakionrails <erick_nara@icloud.com>
2022-06-22 15:42:02 -0300

    initial commit

---
+++ os/src/test/java/io/github/kawaki/yast/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+package io.github.kawaki.yast
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+}
