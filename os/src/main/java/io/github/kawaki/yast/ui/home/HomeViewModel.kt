40c874e993e8409d1afcb6b2c53084a1d1ae4734
kawakionrails <erick_nara@icloud.com>
2022-06-27 09:59:35 -0300

    add: Navigation component and home fragment/viewmodel

---
+++ os/src/main/java/io/github/kawaki/yast/ui/home/HomeViewModel.kt
@@ -0,0 +1,12 @@
+package io.github.kawaki.yast.ui.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel() {
+
+    // TODO: Not yet implemented
+
+}
