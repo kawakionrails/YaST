d47b91fb353920b9ad4bb9a8aa2b5ec20ed76c85
kawakionrails <erick_nara@icloud.com>
2022-07-09 18:09:55 -0300

    add: general fragment and viewmodel

---
+++ os/src/main/java/io/github/kawaki/yast/ui/settings/general/GeneralViewModel.kt
@@ -0,0 +1,12 @@
+package io.github.kawaki.yast.ui.settings.general
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GeneralViewModel @Inject constructor() : ViewModel() {
+
+    // TODO: Not yet implemented
+
+}
